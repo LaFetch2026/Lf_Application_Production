a17b94efd5eb7b7e3ef4d5831eb1aa0080190b4d
SonamAgrahari <sonamagrahari11@gmail.com>
2024-02-02 13:21:30 +0530

    added basecontroller file

---
+++ lib/util/common_widgets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:lafetch/util/constants.dart';
+
+getSnackBar(message, {SnackPosition? snackPosition}) {
+  return Get.snackbar(
+    '',
+    message,
+    titleText: Container(),
+    duration: const Duration(seconds: 2),
+    snackPosition: snackPosition ?? SnackPosition.TOP,
+    backgroundColor: colorSecondary,
+    colorText: colorPrimary,
+  );
+}
