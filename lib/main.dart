61809d29935a8287597b3f62e77556c51bb07275
SonamAgrahari <sonamagrahari11@gmail.com>
2024-02-05 17:48:31 +0530

    added font

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:lafetch/splash.dart';
+import 'package:lafetch/screens/splash.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'First Method',
           theme: ThemeData(
+            fontFamily: 'Franklin Gothic',
             primarySwatch: Colors.grey,
             textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
           ),
---
+++ lib/screens/welcomescreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => WelcomeScreenState();
+}
+
+class WelcomeScreenState extends State<WelcomeScreen> {
+  String? token;
+  String? name;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+    );
+  }
+}
