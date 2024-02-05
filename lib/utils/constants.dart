c3f29d9c767616d1958d4a32e80bd038721f7335
SonamAgrahari <sonamagrahari11@gmail.com>
2024-02-05 17:22:48 +0530

    added image and color

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
-const Color colorPrimary = Color(0xFF272828);
-const Color backgroundCard = Color(0x20ffffff);
-const Color colorSecondary = Color(0xFFFEFB4E);
-const Color greyTextColor = Color(0xFF6B6A69);
+const Color colorPrimary = Color(0xFF070707);
+const Color colorSecondary = Color(0xFFE6E4E1);
+const Color textColor = Color(0xFF46413B);
+const Color borderColor = Color(0xFFD6D4D0);
+const Color btnTextColor = Color(0xFF261939);
+const Color greyTextColor = Color(0xFF66625C);
+const Color deepGreytextColor = Color(0xFF4F445F);
+const Color greyColor = Color(0xFFE6E4E1);
+const Color textHintColor = Color(0xFF86827D);
+const Color blackColor = Color(0xFF000000);
+const Color whiteColor = Color(0xFFffffff);
 
 const splashGif = 'assets/splash.gif';
+const backImage = 'assets/images/back_image.png';
+const appNameImage = 'assets/images/app_name_image.png';
+const googleImage = 'assets/images/google.png';
+const facebookImage = 'assets/images/facebook.png';
