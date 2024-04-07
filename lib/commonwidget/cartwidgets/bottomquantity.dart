398b869f7b8074040c9900ad59c3eec855d3f35a
SonamAgrahari <sonamagrahari11@gmail.com>
2024-04-07 21:08:04 +0530

    designed quality change ui

---
+++ lib/commonwidget/cartwidgets/bottomquantity.dart
@@ -0,0 +1,151 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import '../../utils/constants.dart';
+import '../app_text.dart';
+import '../common_widgets.dart';
+
+class BottomQuantity extends StatefulWidget {
+  final Function? onPressedEdit;
+  final List qtyList;
+  final String selectedQty;
+
+  const BottomQuantity({
+    Key? key,
+    this.onPressedEdit,
+    required this.qtyList,
+    required this.selectedQty,
+  }) : super(key: key);
+
+  @override
+  State<BottomQuantity> createState() => _BottomQuantityState();
+}
+
+class _BottomQuantityState extends State<BottomQuantity> {
+  String text = "0";
+  @override
+  void initState() {
+    text = widget.selectedQty;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: whiteTextColor,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16.0), topRight: Radius.circular(16.0)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      "Select Quantity",
+                      style: TextStyle(
+                        color: loginText,
+                        fontSize: 14.sp,
+                        fontFamily: "Franklin Gothic",
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Image.asset(blackCrossImage,
+                          height: 12, width: 12, fit: BoxFit.cover),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: widget.qtyList.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (ctx, index) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              text = widget.qtyList[index];
+                              print(text);
+                              setState(() {});
+                            },
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 4),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: text == widget.qtyList[index]
+                                        ? btnTextColor
+                                        : whiteColor,
+                                    border: Border.all(
+                                      width: 1,
+                                      color: text == widget.qtyList[index]
+                                          ? whiteColor
+                                          : btnTextColor,
+                                    ),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(20))),
+                                width: 30,
+                                height: 30,
+                                child: Center(
+                                  child: AppText(
+                                    textAlign: TextAlign.center,
+                                    text: widget.qtyList[index],
+                                    color: text == widget.qtyList[index]
+                                        ? whiteColor
+                                        : btnTextColor,
+                                    fontSize: 10.sp,
+                                    fontFamily: "Franklin Gothic Regular",
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    }),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: getSingleButton(
+                  label: "Done",
+                  textColor: whiteBorderColor,
+                  backgroundColor: colorPrimary,
+                  //   controller: profileController,
+                  onPressed: () {},
+                  borderColor: colorPrimary),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
