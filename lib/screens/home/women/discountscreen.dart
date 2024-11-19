37aba4faafd46c54c26b5873f334693da2d6253f
SonamAgrahari <sonamagrahari11@gmail.com>
2024-11-19 20:59:20 +0530

    remove unused import

--- lib/screens/home/women/discountscreen.dart
+++ lib/screens/home/women/discountscreen.dart
@@ -5,7 +5,6 @@ import 'dart:convert';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
