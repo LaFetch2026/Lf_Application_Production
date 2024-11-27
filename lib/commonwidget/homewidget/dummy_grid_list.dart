de48d104343c447cc446b3553cb07cf265c2ce2a
SonamAgrahari <sonamagrahari11@gmail.com>
2024-11-27 20:14:58 +0530

    added animation in wishlist screen

--- lib/commonwidget/homewidget/dummy_grid_list.dart
+++ lib/commonwidget/homewidget/dummy_grid_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:lafetch/commonwidget/dummy_container.dart';
 
 class DummyGridList extends StatelessWidget {
@@ -15,117 +16,134 @@ class DummyGridList extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.only(
             left: 16.sp, right: 16.sp, top: 10.sp, bottom: 90.sp),
-        child: GridView.count(
-          shrinkWrap: true,
-          crossAxisCount: 2,
-          scrollDirection: Axis.vertical,
-          padding: EdgeInsets.zero,
-          childAspectRatio: 0.5,
-          physics: const ScrollPhysics(),
-          crossAxisSpacing: 5.sp,
-          mainAxisSpacing: 0.sp,
-          children: List.generate(
-            size,
-            (index) {
-              return Column(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Stack(
+        child: AnimationLimiter(
+          child: GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            scrollDirection: Axis.vertical,
+            padding: EdgeInsets.zero,
+            childAspectRatio: 0.5,
+            physics: const ScrollPhysics(),
+            crossAxisSpacing: 5.sp,
+            mainAxisSpacing: 0.sp,
+            children: List.generate(
+              size,
+              (index) {
+                return AnimationConfiguration.staggeredGrid(
+                  position: index,
+                  duration: const Duration(milliseconds: 375),
+                  columnCount: 2,
+                  child: ScaleAnimation(
+                    child: FadeInAnimation(
+                      child: Column(
                         children: [
-                          Center(
-                            child: Container(
-                              height: (MediaQuery.of(context).size.width / 2) +
-                                  10.sp,
-                              width: (MediaQuery.of(context).size.width / 2) -
-                                  24.sp,
-                              decoration: BoxDecoration(
-                                color: Colors.black.withOpacity(0.04),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Stack(
+                                children: [
+                                  Center(
+                                    child: Container(
+                                      height:
+                                          (MediaQuery.of(context).size.width /
+                                                  2) +
+                                              10.sp,
+                                      width:
+                                          (MediaQuery.of(context).size.width /
+                                                  2) -
+                                              24.sp,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black.withOpacity(0.04),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 22.sp, vertical: 10.sp),
+                                    child: Align(
+                                      alignment: Alignment.topRight,
+                                      child: InkWell(
+                                        child: DummyContainer(
+                                          height: 24,
+                                          width: 24,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 16.sp,
+                                    left: 16.sp,
+                                    child: DummyContainer(
+                                      height: 26,
+                                      width: 80,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 22.sp, vertical: 10.sp),
-                            child: Align(
-                              alignment: Alignment.topRight,
-                              child: InkWell(
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10.sp, vertical: 5.sp),
                                 child: DummyContainer(
-                                  height: 24,
-                                  width: 24,
+                                  height: 10,
+                                  width: 50,
                                 ),
                               ),
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 16.sp,
-                            left: 16.sp,
-                            child: DummyContainer(
-                              height: 26,
-                              width: 80,
-                            ),
+                              Padding(
+                                padding:
+                                    EdgeInsets.symmetric(horizontal: 10.sp),
+                                child: DummyContainer(
+                                  height: 10,
+                                  width: 50,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    top: 10.sp, left: 10.sp, right: 1.sp),
+                                child: Row(
+                                  children: [
+                                    DummyContainer(
+                                      height: 10,
+                                      width: 50,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 5.sp),
+                                      child: DummyContainer(
+                                        height: 10,
+                                        width: 50,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    top: 10.sp, left: 10.sp, right: 10.sp),
+                                child: Row(
+                                  children: [
+                                    DummyContainer(
+                                      height: 14,
+                                      width: 14,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 5.sp),
+                                      child: DummyContainer(
+                                        height: 10,
+                                        width: 50,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 10.sp, vertical: 5.sp),
-                        child: DummyContainer(
-                          height: 10,
-                          width: 50,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10.sp),
-                        child: DummyContainer(
-                          height: 10,
-                          width: 50,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 10.sp, left: 10.sp, right: 1.sp),
-                        child: Row(
-                          children: [
-                            DummyContainer(
-                              height: 10,
-                              width: 50,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 5.sp),
-                              child: DummyContainer(
-                                height: 10,
-                                width: 50,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 10.sp, left: 10.sp, right: 10.sp),
-                        child: Row(
-                          children: [
-                            DummyContainer(
-                              height: 14,
-                              width: 14,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 5.sp),
-                              child: DummyContainer(
-                                height: 10,
-                                width: 50,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ],
-              );
-            },
+                );
+              },
+            ),
           ),
         ),
       ),
--- lib/screens/expressshopping/viewall.dart
+++ lib/screens/expressshopping/viewall.dart
@@ -14,6 +14,7 @@ import '../../commonwidget/catalogwidgets/bottomfiltters.dart';
 import '../../commonwidget/catalogwidgets/bottomsortby.dart';
 import '../../commonwidget/catalogwidgets/bottomwishlist.dart';
 import '../../commonwidget/doublebtn.dart';
+import '../../commonwidget/dummy_container.dart';
 import '../../controller/product_controller.dart';
 import '../../controller/wishlist_controller.dart';
 import '../../utils/constants.dart';
@@ -64,7 +65,132 @@ class ViewAllScreenState extends State<ViewAllScreen> {
         key: scaffoldKey,
         backgroundColor: whiteColor,
         body: Obx(() => productController.isBrandExpressProduct.value
-            ? const DummyGridList()
+            ? Padding(
+                padding: EdgeInsets.only(
+                  left: 16.sp,
+                  right: 16.sp,
+                  top: 10.sp,
+                ),
+                child: GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  scrollDirection: Axis.vertical,
+                  padding: EdgeInsets.zero,
+                  childAspectRatio: 0.5,
+                  physics: const ScrollPhysics(),
+                  crossAxisSpacing: 5.sp,
+                  mainAxisSpacing: 0.sp,
+                  children: List.generate(
+                    6,
+                    (index) {
+                      return Column(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Stack(
+                                children: [
+                                  Center(
+                                    child: Container(
+                                      height:
+                                          (MediaQuery.of(context).size.width /
+                                                  2) +
+                                              10.sp,
+                                      width:
+                                          (MediaQuery.of(context).size.width /
+                                                  2) -
+                                              24.sp,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black.withOpacity(0.04),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 22.sp, vertical: 10.sp),
+                                    child: Align(
+                                      alignment: Alignment.topRight,
+                                      child: InkWell(
+                                        child: DummyContainer(
+                                          height: 24,
+                                          width: 24,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 16.sp,
+                                    left: 16.sp,
+                                    child: DummyContainer(
+                                      height: 26,
+                                      width: 80,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10.sp, vertical: 5.sp),
+                                child: DummyContainer(
+                                  height: 10,
+                                  width: 50,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    EdgeInsets.symmetric(horizontal: 10.sp),
+                                child: DummyContainer(
+                                  height: 10,
+                                  width: 50,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    top: 10.sp, left: 10.sp, right: 1.sp),
+                                child: Row(
+                                  children: [
+                                    DummyContainer(
+                                      height: 10,
+                                      width: 50,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 5.sp),
+                                      child: DummyContainer(
+                                        height: 10,
+                                        width: 50,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    top: 10.sp, left: 10.sp, right: 10.sp),
+                                child: Row(
+                                  children: [
+                                    DummyContainer(
+                                      height: 14,
+                                      width: 14,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 5.sp),
+                                      child: DummyContainer(
+                                        height: 10,
+                                        width: 50,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              )
             : Stack(
                 children: [
                   Positioned.fill(
