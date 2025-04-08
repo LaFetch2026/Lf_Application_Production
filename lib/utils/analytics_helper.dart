8e7f3eb3bb9ed57afc301e2e49ada505347d7397
gouravxpression <gourav.lohat@xpressioncorp.com>
2025-04-08 14:48:20 +0530

    analytics.dart

--- lib/utils/analytics_helper.dart
+++ lib/utils/analytics_helper.dart
@@ -14,12 +14,12 @@ class AnalyticsHelper {
   }
 
   /// View Content - e.g. Product page opened
-  static void logContentView({required String id, required double value}) {
+  static void logContentView({required String productId, required double value}) {
     facebookAppEvents.logEvent(
       name: 'fb_mobile_content_view',
       parameters: {
         'content_type': 'product',
-        'content_id': id,
+        'content_id': productId,
         'currency': 'USD',
         'value': value,
       },
@@ -51,45 +51,45 @@ class AnalyticsHelper {
   }
 
   /// Add to Cart - User adds product to cart
-  static void logAddToCart({required String id, required double value}) {
+  static void logAddToCart({required String productId, required double totalProductValue}) {
     facebookAppEvents.logEvent(
       name: 'fb_mobile_add_to_cart',
       parameters: {
         'content_type': 'product',
-        'content_id': id,
+        'content_id': productId,
         'currency': 'USD',
-        'value': value,
+        'value': totalProductValue,
       },
     );
   }
 
   /// Initiate Checkout - Checkout started
   static void logInitiateCheckout({
-    required List<String> productIds,
-    required double totalValue,
+    required List<String> productId,
+    required double totalProductValue,
     bool paymentInfoAvailable = false,
   }) {
     facebookAppEvents.logEvent(
       name: 'fb_mobile_initiated_checkout',
       parameters: {
         'content_type': 'product',
-        'content_id': productIds.join(','),
-        'num_items': productIds.length,
+        'content_id': productId.join(','),
+        'num_items': productId.length,
         'payment_info_available': paymentInfoAvailable,
         'currency': 'USD',
-        'value': totalValue,
+        'value': totalProductValue,
       },
     );
   }
 
   /// Purchase - After successful order
-  static void logPurchase({required String id, required double value}) {
+  static void logPurchase({required String productId, required double totalProductValue}) {
     facebookAppEvents.logPurchase(
-      amount: value,
+      amount: totalProductValue,
       currency: 'USD',
       parameters: {
         'content_type': 'product',
-        'content_id': id,
+        'content_id': productId,
       },
     );
   }
